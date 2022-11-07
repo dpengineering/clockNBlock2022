913c77718a21d110571901d25d5f6f916763cd78
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2022-11-07 12:51:20 -0800

    Fixed typo in setup

--- oneTimeSetup/LinearDeltaRobotSetup.py
+++ oneTimeSetup/LinearDeltaRobotSetup.py
@@ -6,7 +6,7 @@
 #      *                                                                *
 #      ******************************************************************
 
-from dpeaDPi.DPi_Robot import DPi_Robot
+from dpeaDPi.DPiRobot import DPiRobot
 from time import sleep
 import math
 
@@ -34,7 +34,7 @@ microStepping = 32
 reverseStepDirectionFlag = True
 
 
-def main(): 
+def main():
     #
     # Up to 4 DPiRobot boards can be connected to a DPiNetwork.  Each board is addressed
     # (0 - 3) by configuring its jumpers.  If no jumpers are installed, the board number
@@ -45,6 +45,37 @@ def main():
     # In this example, we will be using board number 0
     #
     dpiRobot.setBoardNumber(0)
+
+    # Initalize the robot to the default values
+    if dpiRobot.initialize() != True:
+        print("Communication with the DPiRobot board failed.")
+        return
+
+    #
+    # At all times the robot has a "Status" value that you can get by calling getRobotStatus().
+    # Two values are return and assigned to the variables "success_flg" and "status".
+    # "success_flg" will be "True" if the command executed successfully or "False" if it
+    # failed while trying to get the status.  "status" can have many values depending on what
+    # the robot is doing.
+    #
+    _success_flg, status = dpiRobot.getRobotStatus()
+
+    if status == dpiRobot.STATE_NOT_READY:
+        print("Status: Robot is not ready.")
+    elif status == dpiRobot.STATE_MOTORS_DISABLED:
+        print("Status: Motors are disabled.")
+    elif status == dpiRobot.STATE_NOT_HOMED:
+        print("Status: Motors enabled but robot not homed.")
+    elif status == dpiRobot.STATE_HOMING:
+        print("Status: Robot is running the homing procedure.")
+    elif status == dpiRobot.STATE_STOPPED:
+        print("Status: Robot is stopped.")
+    elif status == dpiRobot.STATE_PREPARING_TO_MOVE:
+        print("Status: Robot received 1 or more waypoints but hasn't started moving.")
+    elif status == dpiRobot.STATE_MOVING:
+        print("Status: Robot is moving.")
+    else:
+        print("Unknown status.")
 
     #Initalize the Delta Arm
 
