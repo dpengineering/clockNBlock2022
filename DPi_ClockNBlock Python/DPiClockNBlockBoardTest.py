fc3930c11349b33c63cf8b7b81ab8c3852d14d67
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2022-11-28 22:28:36 -0800

    Created very rough test for the DPi ClockNBlock firmware

---
+++ DPi_ClockNBlock Python/DPiClockNBlockBoardTest.py
@@ -0,0 +1,24 @@
+# Rough test for functionality of the DPi ClockNBlock board
+
+from DPiClockNBlock import DPiClockNBlock
+from time import sleep
+
+
+def main():
+    dpiClockNBlock = DPiClockNBlock()
+
+    dpiClockNBlock.setBoardNumber(0)
+
+    if not dpiClockNBlock.initialize():
+        print("Communication with board failed")
+        return
+
+    dpiClockNBlock.arrowOn()
+
+    while True:
+        print(dpiClockNBlock.readEntrance())
+        sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
