a2cbe374d016b663a32febed250495d2912d6865
Brian Vesper <brianvesper@dpengineering.org>
2022-12-09 15:10:44 -0800

    think everything is fixed but idk sometimes git just goes ham and idk why

--- main/robotArm.py
+++ main/robotArm.py
@@ -10,15 +10,11 @@ import sys
 
 from dpeaDPi.DPiRobot import DPiRobot
 from dpeaDPi.DPiSolenoid import DPiSolenoid
-<<<<<<< HEAD
 from time import sleep
 from time import gmtime, strftime
 import tty, sys, termios
-=======
 from time import sleep, gmtime, strftime
-import pygame
 
->>>>>>> 965bf1936c6123a9269991751ee83dafc5f41195
 # Constants go here
 
 dpiRobot = DPiRobot()
@@ -66,10 +62,7 @@ class RobotArm:
         return r, theta, z
 
     def train(self):
-<<<<<<< HEAD
         pos = dpiRobot.getCurrentPosition()
-=======
->>>>>>> 965bf1936c6123a9269991751ee83dafc5f41195
         speed = 40
         magnet = False
         rotation = False
@@ -80,8 +73,6 @@ class RobotArm:
         filedescriptors = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin)
         while (True):
-<<<<<<< HEAD
-
             x = sys.stdin.read(1)[0]
             print(x)
             if x == 'a':
@@ -117,84 +108,6 @@ class RobotArm:
                 break
 
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
-=======
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    pos = dpiRobot.getCurrentPosition()
-                    if event.key == pygame.K_LEFT:
-                        # Move robot a little to the left
-                        print('-x')
-                        dpiRobot.addWaypoint(pos[1] - 0.5, pos[2], pos[3], speed)
-                    if event.key == pygame.K_RIGHT:
-                        print('+x')
-                        dpiRobot.addWaypoint(pos[1] + 0.5, pos[2], pos[3], speed)
-                    if event.key == pygame.K_UP:
-                        print('+y')
-                        dpiRobot.addWaypoint(pos[1], pos[2] + 0.5, pos[3], speed)
-                    if event.key == pygame.K_DOWN:
-                        print('-y')
-                        dpiRobot.addWaypoint(pos[1], pos[2] - 0.5, pos[3], speed)
-                    if event.key == pygame.K_z:
-                        print('-z')
-                        dpiRobot.addWaypoint(pos[1], pos[2], pos[3]-0.5, speed)
-                    if event.key == pygame.K_x:
-                        print('z')
-                        dpiRobot.addWaypoint(pos[1], pos[2], pos[3]+0.5, speed)
-                    if event.key == pygame.K_SPACE:
-                        print("magnet")
-                        dpiSolenoid.switchDriverOnOrOff(not magnet)
-                        magnet = not magnet
-                    if event.key == pygame.K_r:
-                        print('rotate')
-                        dpiSolenoid.switchDriverOnOrOff(not rotation)
-                        rotation = not rotation
-                    if event.key == pygame.K_s:
-                        print("write")
-                        locationsFile = open("locations.txt", "w")
-                        name = input("What point is this")
-                        pos = self.cartesianToPolar(dpiRobot.getCurrentPosition())
-                        locationsFile.write(f'{name}: {pos} \n')
-                        locationsFile.close()
-                    if event.key == pygame.K_ESCAPE:
-                        print("done")
-                        pygame.quit()
-                        return
-
-
-# def testKeys():
-#
-#     while (True):
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_LEFT:
-#                     print('-x')
-#                 if event.key == pygame.K_RIGHT:
-#                     print('+x')
-#                 if event.key == pygame.K_UP:
-#                     print('+y')
-#                 if event.key == pygame.K_DOWN:
-#                     print('-y')
-#                 if event.key == pygame.K_z:
-#                     print('-z')
-#                 if event.key == pygame.K_x:
-#                     print('z')
-#                 if event.key == pygame.K_SPACE:
-#                     print("magnet")
-#                 if event.key == pygame.K_r:
-#                     print('rotate')
-#                 if event.key == pygame.K_s:
-#                     print("write")
-#                 if event.key == pygame.K_ESCAPE:
-#                     print("done")
-#                     pygame.quit()
-#                     return
->>>>>>> 965bf1936c6123a9269991751ee83dafc5f41195
 
 def main():
     robotArm = RobotArm(11, 10)
