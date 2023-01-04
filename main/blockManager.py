8d791185a6a1a9f9427a9a3cdb2895f0d7994521
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2023-01-04 00:34:21 -0800

    debugging

--- main/blockManager.py
+++ main/blockManager.py
@@ -89,8 +89,8 @@ class BlockManager:
 
     def isReady(self, clockPos: float) -> bool:
 
-        minPos = self.buildPos[1] - 0.6
-        maxPos = self.feederPos[1] + 0.6
+        minPos = self.buildPos[1] + 0.6
+        maxPos = self.feederPos[1] - 0.6
         if minPos < clockPos < maxPos:
             self.resetStack()
             return False
