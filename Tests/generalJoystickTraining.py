a5b078d76088c86f39196a460e6655d753761216
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2023-01-30 20:59:30 -0800

    Made a general way to move the robot with a joystick

---
+++ Tests/generalJoystickTraining.py
@@ -0,0 +1,105 @@
+#      ******************************************************************
+#      *                                                                *
+#      *                 Trains robot using joystick                    *
+#      *                                                                *
+#      *            Arnav Wadhwa                     1/30/2023          *
+#      *                                                                *
+#      ******************************************************************
+from time import strftime, gmtime
+
+import pygame
+import os
+from dpeaDPi.DPiRobot import DPiRobot
+
+
+class TrainingCartesian:
+
+    # YOU CAN CRASH THE ROBOT WITH THIS, PLEASE DON'T DRIVE IT RECKLESSLY
+
+    # Empty array of waypoints at the moment,
+    buffer = [[0] * 3] * 3
+    locationsFile = open("locations", "a")
+    time = strftime("%Y-%m-%d %H:%M", gmtime())
+    locationsFile.write(f'Locations saved at {time} \n')
+    locationsFile.close()
+
+    def __init__(self):
+
+        # allow ssh deploy with pygame (no screen)
+        os.environ['SDL_VIDEODRIVER'] = "dummy"
+
+        pygame.init()
+        pygame.joystick.init()
+        self.joystick = pygame.joystick.Joystick(0)
+        self.joystick.init()
+
+        self.dpiRobot = DPiRobot()
+
+    def get_axis(self, axis):
+        """
+        Get the axis (x or y) of the joystick.
+        :raises: ValueError If the given axis isn't 'x' 'y'
+        :param axis: axis to get value of
+        :rtype: float
+        :return: All the way to the right=1, fully up=-1
+        """
+        axis.lower()
+        self.refresh()
+        if axis == "x":
+            return self.joystick.get_axis(0)
+        elif axis == "y":
+            return self.joystick.get_axis(1)
+        elif axis == "z":
+            return self.joystick.get_axis(2)
+        else:
+            raise ValueError("Axis must be of type str and either 'x', 'y', or 'z'")
+
+    @staticmethod
+    def refresh():
+        """
+        Refresh the joysticks current value
+        :return: None
+        """
+        pygame.event.pump()
+
+    @staticmethod
+    def num_to_range(num, inMin, inMax, outMin, outMax):
+
+        return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax - outMin))
+
+    def generate_waypoint(self):
+
+        # Currently, the 5 is an arbitrary number, change it as you wish
+        translateX = self.num_to_range(self.get_axis('x'), -1, 1, -5, 5)
+        translateY = self.num_to_range(self.get_axis('y'), -1, 1, -5, 5)
+        translateZ = self.num_to_range(self.get_axis('z'), 1, -1, -60, 200)
+
+        currentX, currentY, currentZ = self.dpiRobot.getCurrentPosition()
+
+        return translateX + currentX, translateY + currentY, translateZ
+
+    def add_waypoint(self, waypoint: tuple, speed: int = 50):
+        self.dpiRobot.addWaypoint(waypoint[0], waypoint[1], waypoint[2], speed)
+
+    def check_other_buttons(self):
+        for event in pygame.event.get():
+            if event.type == pygame.JOYBUTTONDOWN:
+                if self.joystick.get_button(0):
+                    print("write to file")
+                    locationsFile = open("locations", "a")
+                    name = input("What point is this")
+                    status, x, y, z = self.dpiRobot.getCurrentPosition()
+                    locationsFile.write(f'{name}: ({x}, {y}, {z} \n')
+                    locationsFile.close()
+
+
+def main():
+    joystick = TrainingCartesian()
+    joystick.dpiRobot.bufferWaypointsBeforeStartingToMove(True)
+    while True:
+        joystick.add_waypoint(joystick.generate_waypoint())
+        joystick.check_other_buttons()
+
+
+if __name__ == "__main__":
+    main()
