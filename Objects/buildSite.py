542c370bdfa5971027e5d47900a5d1722a3c3e01
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2023-04-18 11:23:20 -0700

    debugging branch created

--- Objects/buildSite.py
+++ Objects/buildSite.py
@@ -33,6 +33,7 @@ class BuildSite:
         # Our setup is fairly simple, we just need to find the block placement list we have
         # and reset our counter to 0
         self.blockPlacements = self.generatePlacementList(constants.placementArrays[self.index], self.location0)
+        print(f'Placement List: {self.blockPlacements}, buildSite: {self.index}')
         self.currentBlock = 0
         self.isReadyFlg = True
 
