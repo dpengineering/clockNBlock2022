070fb971fa3a4b0c0430c11dcf60fbdcea481c7b
ArnavVWadhwa <arnav.v.wadhwa@gmil.com>
2023-04-24 09:46:06 -0700

    Updated testClockMovement.py

--- Tests/testClockMovement.py
+++ Tests/testClockMovement.py
@@ -3,10 +3,17 @@ sys.path.insert(0, '../')
 
 from Objects.clock import Clock
 
+from dpeaDPi.DPiRobot import DPiRobot
+
 clock = Clock()
+dpiRobot = DPiRobot()
+dpiRobot.setBoardNumber(0)
+dpiRobot.initialize()
 
 
 def main():
+
+    dpiRobot.homeRobot(True)
     clock.setup()
     clock.setup2()
 
